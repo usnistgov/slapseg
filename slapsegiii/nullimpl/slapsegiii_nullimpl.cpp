236dccfda158d415fa6d95b9b9a600f928278c5b
Gregory Fiumara <gregory.fiumara@nist.gov>
2019-04-18 10:34:11 -0400

    Add option to randomly fail in NullImplementation.

--- slapsegiii/nullimpl/slapsegiii_nullimpl.cpp
+++ slapsegiii/nullimpl/slapsegiii_nullimpl.cpp
@@ -8,6 +8,13 @@
  * about its quality, reliability, or any other characteristic.
  */
 
+#define RANDOMLY_FAIL
+
+#ifdef RANDOMLY_FAIL
+#include <random>
+#include <type_traits>
+#endif
+
 #include <slapsegiii_nullimpl.h>
 
 SlapSegIII::SubmissionIdentification
@@ -46,6 +53,151 @@ SlapSegIII::NullImplementation::segment(
 
 	positions.reserve(
 	    image.orientation == SlapImage::Orientation::Thumbs ? 2 : 4);
+
+	#ifdef RANDOMLY_FAIL
+	static std::random_device rd{};
+	static std::default_random_engine generator{rd()};
+	static std::uniform_int_distribution<uint8_t> overallFail(1, 100);
+	static std::uniform_int_distribution<uint8_t> positionFail(1, 4);
+
+	const auto failCode = overallFail(generator);
+	/* Randomly fail to process image */
+	if (failCode == 1) {
+		return (std::make_tuple(ReturnStatus(
+		    ReturnStatus::Code::VendorDefined, {}, "Randomly failing"),
+		    positions));
+	/* Randomly fail to find a finger in an image */
+	} else if (failCode == 50) {
+		const auto posFailCode = positionFail(generator);
+
+		SegmentationPosition failedPos{};
+		failedPos.result = SegmentationPosition::Result(
+		    SegmentationPosition::Result::Code::FingerNotFound,
+		    "Randomly not finding finger");
+
+		switch (image.orientation) {
+		case SlapImage::Orientation::Thumbs:
+			if (posFailCode == 1) {
+				failedPos.frgp =
+				    FrictionRidgeGeneralizedPosition::LeftThumb;
+				positions.push_back(failedPos);
+			} else {
+				positions.emplace_back(
+				    FrictionRidgeGeneralizedPosition::LeftThumb,
+				    Coordinate{0, 0}, Coordinate{10, 0},
+				    Coordinate{0, 10}, Coordinate{10, 10});
+			}
+			if (posFailCode == 2) {
+				failedPos.frgp =
+				    FrictionRidgeGeneralizedPosition::RightThumb;
+				positions.push_back(failedPos);
+			} else {
+				positions.emplace_back(
+				    FrictionRidgeGeneralizedPosition::RightThumb,
+				    Coordinate{0, 0}, Coordinate{10, 0},
+				    Coordinate{0, 10}, Coordinate{10, 10});
+			}
+			break;
+		case SlapImage::Orientation::Right:
+			if (posFailCode == 1) {
+				failedPos.frgp =
+				    FrictionRidgeGeneralizedPosition::RightIndex;
+				positions.push_back(failedPos);
+			} else {
+				positions.emplace_back(
+				    FrictionRidgeGeneralizedPosition::RightIndex,
+				    Coordinate{0, 0}, Coordinate{10, 0},
+				    Coordinate{0, 10}, Coordinate{10, 10});
+			}
+			if (posFailCode == 2) {
+				failedPos.frgp =
+				    FrictionRidgeGeneralizedPosition::
+				    RightMiddle;
+				positions.push_back(failedPos);
+			} else {
+				positions.emplace_back(
+				    FrictionRidgeGeneralizedPosition::
+				    RightMiddle, Coordinate{0, 0},
+				    Coordinate{10, 0}, Coordinate{0, 10},
+				    Coordinate{10, 10});
+			}
+			if (posFailCode == 3) {
+				failedPos.frgp =
+				    FrictionRidgeGeneralizedPosition::RightRing;
+				positions.push_back(failedPos);
+			} else {
+				positions.emplace_back(
+				    FrictionRidgeGeneralizedPosition::RightRing,
+				    Coordinate{0, 0}, Coordinate{10, 0},
+				    Coordinate{0, 10}, Coordinate{10, 10});
+			}
+			if (posFailCode == 4) {
+				failedPos.frgp =
+				    FrictionRidgeGeneralizedPosition::
+				    RightLittle;
+				positions.push_back(failedPos);
+			} else {
+				positions.emplace_back(
+				    FrictionRidgeGeneralizedPosition::
+				    RightLittle, Coordinate{0, 0},
+				    Coordinate{10, 0}, Coordinate{0, 10},
+				    Coordinate{10, 10});
+			}
+			break;
+		case SlapImage::Orientation::Left:
+			if (posFailCode == 1) {
+				failedPos.frgp =
+				    FrictionRidgeGeneralizedPosition::LeftIndex;
+				positions.push_back(failedPos);
+			} else {
+				positions.emplace_back(
+				    FrictionRidgeGeneralizedPosition::LeftIndex,
+				    Coordinate{0, 0}, Coordinate{10, 0},
+				    Coordinate{0, 10}, Coordinate{10, 10});
+			}
+			if (posFailCode == 2) {
+				failedPos.frgp =
+				    FrictionRidgeGeneralizedPosition::
+				    LeftMiddle;
+				positions.push_back(failedPos);
+			} else {
+				positions.emplace_back(
+				    FrictionRidgeGeneralizedPosition::
+				    LeftMiddle, Coordinate{0, 0},
+				    Coordinate{10, 0}, Coordinate{0, 10},
+				    Coordinate{10, 10});
+			}
+			if (posFailCode == 3) {
+				failedPos.frgp =
+				    FrictionRidgeGeneralizedPosition::LeftRing;
+				positions.push_back(failedPos);
+			} else {
+				positions.emplace_back(
+				    FrictionRidgeGeneralizedPosition::LeftRing,
+				    Coordinate{0, 0}, Coordinate{10, 0},
+				    Coordinate{0, 10}, Coordinate{10, 10});
+			}
+			if (posFailCode == 4) {
+				failedPos.frgp =
+				    FrictionRidgeGeneralizedPosition::
+				    LeftLittle;
+				positions.push_back(failedPos);
+			} else {
+				positions.emplace_back(
+				    FrictionRidgeGeneralizedPosition::
+				    LeftLittle, Coordinate{0, 0},
+				    Coordinate{10, 0}, Coordinate{0, 10},
+				    Coordinate{10, 10});
+			}
+			break;
+		}
+
+		return (std::make_tuple(ReturnStatus(
+		    ReturnStatus::Code::RequestRecaptureWithAttempt,
+		    {SlapImage::Deficiency::HandGeometry}, "Randomly failing"),
+		    positions));
+	}
+	#endif
 
 	switch (image.orientation) {
 	case SlapImage::Orientation::Thumbs:
